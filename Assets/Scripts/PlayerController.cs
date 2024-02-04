acbc24e007a3a2ddc620463e0500141e6e3592f0
Dominik <dodo.stefan31@gmail.com>
2024-02-04 20:46:38 +0100

    added level up promijenio health stvari dodao exp counter i exp potreban za level up koji se poveća nakon level up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public int health = 5;
+    public int playerHealth = 100;
+    public int playerDamage = 10;
     private Vector2 mov;
     private Rigidbody2D rb;
+    private int level = 1;
+    private int exp = 0;
+
+    private int expToLevelUp = 100;
 
     void Start()
     {
@@ -22,5 +27,25 @@ public class PlayerController : MonoBehaviour
     {
         mov = mov.normalized * Time.fixedDeltaTime * moveSpeed;
         rb.MovePosition(rb.position + mov);
+    }
+
+    public void AddExpToPlayer(int expGained)
+    {
+        exp += expGained;
+
+        Debug.Log("Exp: " + exp);
+
+        ExpChecker();
+    }
+
+    private void ExpChecker()
+    {
+        if (exp >= expToLevelUp)
+        {
+            level++;
+            exp -= expToLevelUp;
+            expToLevelUp = expToLevelUp + (int)(expToLevelUp * 0.1f);
+            Debug.Log("Level Up! ExpToLevelUp:" + expToLevelUp);
+        }
     }
 }
