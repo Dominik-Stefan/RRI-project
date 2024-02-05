d95972f3c24895193c842f3ef05bff6162154d04
Dominik <dodo.stefan31@gmail.com>
2024-02-05 10:31:32 +0100

    added placeholder options

--- Assets/Scripts/UI/LevelUpMenuController.cs
+++ Assets/Scripts/UI/LevelUpMenuController.cs
@@ -1,5 +1,6 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.UIElements;
 
 public class LevelUpMenuController : MonoBehaviour
@@ -8,10 +9,17 @@ public class LevelUpMenuController : MonoBehaviour
     private Button buttonConfirm;
     private PlayerController playerController;
     private GameController gameController;
+    private RadioButtonGroup levelUpOptions;
 
     private void OnEnable()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
+
+        List<string> choices = new List<string> { "Option 1", "Option 2", "Option 3" };
+
+        levelUpOptions = root.Q<RadioButtonGroup>("LevelUpOptions");
+        levelUpOptions.choices = choices;
+        levelUpOptions.value = 0;
 
         buttonConfirm = root.Q<Button>("ConfirmButton");
 
