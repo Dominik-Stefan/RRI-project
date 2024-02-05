012970a39fdb628e916e8e658929263500d7f9ca
Kristijan <kjagunec@gmail.com>
2024-02-05 18:18:32 +0100

    No conflict plz #2

--- Assets/Scripts/ShootingController.cs
+++ Assets/Scripts/ShootingController.cs
@@ -11,12 +11,12 @@ public class ShootingController : MonoBehaviour
     private Vector3 localMousePosition;
     private Vector3 mousePosition;
     private GameController gameController;
-    private PlayerController playerController;
+    //private PlayerController playerController;
 
     void Start()
     {
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
-        playerController = GetComponentInParent<PlayerController>();
+        //playerController = GetComponentInParent<PlayerController>();
         canFire = true;
     }
 
