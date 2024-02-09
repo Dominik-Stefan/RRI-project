e2f88ea4352778c03caed9a0fdc710976e85b582
Mislav <maljkovic.mislav1@gmail.com>
2024-02-09 01:53:10 +0100

    Scuffed Wave system, kraj nije imlementan
    Igra traje 15 minuta, wave promjena svake 3

--- Assets/Scripts/Enemy/Base/EnemySpawnerController.cs
+++ Assets/Scripts/Enemy/Base/EnemySpawnerController.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Waves;
+
 public class EnemySpawnerController : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject bombPrefab;
+    public GameObject treasurePrefab;
     public float interval = 100;
     public float spawnRadius = 20;
     private float counter = 0;
+    private List<Wave> allWaves = new List<Wave> { new Wave5(), new Wave2(), new Wave3(), new Wave4(), new Wave5()};
+    private int chosenWave = 0;
+
+    void Start(){
+        StartCoroutine(ManageWaves());
+    }
+
+    IEnumerator ManageWaves(){
+        for (int i = 0; i < allWaves.Count - 1; i++)
+        {
+            yield return new WaitForSeconds(180f);
+            chosenWave += 1;
+        }
+        chosenWave -= 1;
+    }
 
     void FixedUpdate()
     {
@@ -16,14 +34,69 @@ public class EnemySpawnerController : MonoBehaviour
         if (counter >= interval)
         {
             counter = 0;
-            Vector2 spawnPosition = (Vector2)transform.position - Random.insideUnitCircle.normalized * spawnRadius;
 
-            //Instantiate(enemyPrefab, spawnPosition, transform.rotation);
-            GameObject enemy = EnemyObjectPool.SharedInstance.getPooledEnemyObject();
-            if(enemy != null){
-                enemy.transform.position = spawnPosition;
-                enemy.transform.rotation = transform.rotation;
-                enemy.SetActive(true);
+            Vector2 spawnPosition = (Vector2)transform.position - Random.insideUnitCircle.normalized * spawnRadius;
+            string nameOfObject = null;
+            int rand = Random.Range(1, 100);
+            bool found = false;
+            int sum = 0;
+
+            if(rand <= allWaves[chosenWave].bombChance && !found){
+                Instantiate(bombPrefab, spawnPosition, transform.rotation);
+                found = true;
+            }
+            sum += allWaves[chosenWave].bombChance;
+
+            if(rand <= sum + allWaves[chosenWave].treasureChance && !found){
+                Instantiate(treasurePrefab, spawnPosition, transform.rotation);
+                found = true;
+            }
+            sum += allWaves[chosenWave].treasureChance;
+
+            if(rand <= sum + allWaves[chosenWave].gunslinger1Chance && !found){
+                nameOfObject = "Enemy_Gunman1(Clone)";
+                found = true;
+            }
+            sum += allWaves[chosenWave].gunslinger1Chance;
+
+            if(rand <= sum + allWaves[chosenWave].gunslinger2Chance && !found){
+                nameOfObject = "Enemy_Gunman2(Clone)";
+                found = true;
+            }
+            sum += allWaves[chosenWave].gunslinger2Chance;
+
+            if(rand <= sum + allWaves[chosenWave].gunslinger3Chance && !found){
+                nameOfObject = "Enemy_Gunman3(Clone)";
+                found = true;
+            }
+            sum += allWaves[chosenWave].gunslinger3Chance;
+
+            if(rand <= sum + allWaves[chosenWave].slime1Chance && !found){
+                nameOfObject = "Enemy_Slime1(Clone)";
+                found = true;
+            }
+            sum += allWaves[chosenWave].slime1Chance;
+
+            if(rand <= sum + allWaves[chosenWave].slime2Chance && !found){
+                nameOfObject = "Enemy_Slime2(Clone)";
+                found = true;
+            }
+            sum += allWaves[chosenWave].slime2Chance;
+
+            if(rand <= sum + allWaves[chosenWave].slime3Chance && !found){
+                nameOfObject = "Enemy_Slime3(Clone)";
+                found = true;
+            }
+
+            Debug.Log(nameOfObject);
+
+            if(nameOfObject != null){
+                GameObject enemy = EnemyObjectPool.SharedInstance.getPooledEnemyObject(nameOfObject);
+                if(enemy != null){
+                    enemy.transform.position = spawnPosition;
+                    enemy.transform.rotation = transform.rotation;
+                    enemy.SetActive(true);
+                }
             }
         }
     }
--- Assets/Scripts/ObjectPool/EnemyObjectPool.cs
+++ Assets/Scripts/ObjectPool/EnemyObjectPool.cs
@@ -6,7 +6,13 @@ public class EnemyObjectPool : MonoBehaviour
 {
     public static EnemyObjectPool SharedInstance;
     public List<GameObject> pooledEnemyObjects;
-    public GameObject objectToPool;
+    public GameObject objectToPool1;
+    public GameObject objectToPool2;
+    public GameObject objectToPool3;
+    public GameObject objectToPool4;
+    public GameObject objectToPool5;
+    public GameObject objectToPool6;
+
     public int amountToPool;
 
     void Awake(){
@@ -15,20 +21,32 @@ public class EnemyObjectPool : MonoBehaviour
 
     void Start(){
         pooledEnemyObjects = new List<GameObject>();
+        AddToPool(objectToPool1);
+        AddToPool(objectToPool2);
+        AddToPool(objectToPool3);
+        AddToPool(objectToPool4);
+        AddToPool(objectToPool5);
+        AddToPool(objectToPool6);
+    }
+
+    private void AddToPool(GameObject objects){
         GameObject tmp;
         for(int i = 0; i < amountToPool; i++){
-            tmp = Instantiate(objectToPool);
+            tmp = Instantiate(objects);
             tmp.SetActive(false);
             pooledEnemyObjects.Add(tmp);
         }
     }
 
-    public GameObject getPooledEnemyObject(){
-
-        List<GameObject> inactiveObjects = pooledEnemyObjects.FindAll( go => !go.activeInHierarchy );
-
-        return inactiveObjects.Count > 0 ?
-         inactiveObjects[ Random.Range(0, inactiveObjects.Count) ] :
-         null;
+    public GameObject getPooledEnemyObject(string objectName){
+        if(pooledEnemyObjects.FindAll( go => !go.activeInHierarchy ).Count > 250){
+            List<GameObject> newList = pooledEnemyObjects.FindAll(x => x.name == objectName);
+            for(int i = 0; i < amountToPool; i++){
+                if(!newList[i].activeInHierarchy){
+                    return newList[i];
+                }
+            }   
+        }
+        return null;
     }
 }
