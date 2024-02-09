bbc07ee92f58963a9a87845279f7cf8062813496
Kristijan <kjagunec@gmail.com>
2024-02-09 17:56:13 +0100

    Updated tilemap

--- Assets/Scripts/EndlessTilemap.cs
+++ Assets/Scripts/EndlessTilemap.cs
@@ -11,27 +11,23 @@ public class EndlessTilemap : MonoBehaviour
     private int chunksVisible;
     private Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
     private List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+    private List<TerrainChunk> terrainChunksVisibleThisUpdate = new List<TerrainChunk>();
 
     void Start()
     {
         chunksVisible = Mathf.RoundToInt(maxViewDistance / chunkSize);
-        Debug.Log(chunksVisible);
     }
 
     void Update()
     {
         playerPosition = new Vector2(playerTransform.position.x, playerTransform.position.y);
-        Debug.Log(playerPosition);
         UpdateVisibleChunks();
     }
 
     void UpdateVisibleChunks()
     {
-        for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++)
-        {
-            terrainChunksVisibleLastUpdate[i].SetVisible(false);
-        }
-        terrainChunksVisibleLastUpdate.Clear();
+        terrainChunksVisibleLastUpdate = new List<TerrainChunk>(terrainChunksVisibleThisUpdate);
+        terrainChunksVisibleThisUpdate.Clear();
 
         int currentChunkCoordX = Mathf.RoundToInt(playerPosition.x / chunkSize);
         int currentChunkCoordY = Mathf.RoundToInt(playerPosition.y / chunkSize);
@@ -41,20 +37,38 @@ public class EndlessTilemap : MonoBehaviour
             for (int xOffset = -chunksVisible; xOffset <= chunksVisible; xOffset++)
             {
                 Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+                TerrainChunk currentTerrainChunk;
 
                 if (terrainChunkDictionary.ContainsKey(viewedChunkCoord))
                 {
-                    terrainChunkDictionary[viewedChunkCoord].UpdateTerrainChunk();
-                    if (terrainChunkDictionary[viewedChunkCoord].IsVisible())
+                    currentTerrainChunk = terrainChunkDictionary[viewedChunkCoord];
+                    currentTerrainChunk.UpdateTerrainChunk();
+                    if (currentTerrainChunk.IsVisible())
                     {
-                        terrainChunksVisibleLastUpdate.Add(terrainChunkDictionary[viewedChunkCoord]);
+                        terrainChunksVisibleThisUpdate.Add(currentTerrainChunk);
                     }
                 }
                 else
                 {
-                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, tilemapPrefab, transform));
+                    currentTerrainChunk = new TerrainChunk(viewedChunkCoord, chunkSize, tilemapPrefab, transform);
+                    terrainChunkDictionary.Add(viewedChunkCoord, currentTerrainChunk);
                 }
             }
+        }
+
+        for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++)
+        {
+            bool visible = false;
+            for (int j = 0; j < terrainChunksVisibleThisUpdate.Count; j++)
+            {
+                if (terrainChunksVisibleLastUpdate[i].Equals(terrainChunksVisibleThisUpdate[j]))
+                {
+                    visible = true;
+                    break;
+                }
+            }
+
+            terrainChunksVisibleLastUpdate[i].SetVisible(visible);
         }
     }
 
