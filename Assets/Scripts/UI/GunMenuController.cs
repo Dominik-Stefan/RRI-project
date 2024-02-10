886cf429b954402a5d819fc6ae4db2726864d248
Kristijan <kjagunec@gmail.com>
2024-02-10 13:11:08 +0100

    Updated Gun menu

--- Assets/Scripts/UI/GunMenuController.cs
+++ Assets/Scripts/UI/GunMenuController.cs
@@ -13,10 +13,12 @@ public class GunMenuController : MonoBehaviour
         Button buttonPistol = root.Q<Button>("PistolButton");
         Button buttonShotgun = root.Q<Button>("ShotgunButton");
         Button buttonMinigun = root.Q<Button>("MinigunButton");
+        Button buttonCancel = root.Q<Button>("CancelButton");
 
         buttonPistol.clicked += () => GunPicker("Pistol");
         buttonShotgun.clicked += () => GunPicker("Shotgun");
         buttonMinigun.clicked += () => GunPicker("Minigun");
+        buttonCancel.clicked += () => gunMenuUIDocument.rootVisualElement.style.display = DisplayStyle.None;
     }
 
     private void GunPicker(string gunName)
